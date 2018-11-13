524c84cc64f24eb8f1c10ea106d40474fc0a6f9d
Fred Sauvage <frederic.sauv@gmail.com>
2018-11-13 14:32:44 +0000

    Clean example

--- optim/example_bouncing_atom.py
+++ optim/example_bouncing_atom.py
@@ -32,29 +32,37 @@ limits = (0.8, 1.2)
 bounds = [{'name': 'omega_ratio', 'type': 'continuous', 'domain': limits}]
 
 # define functions FoM functions
+# no noise
 f = lambda x: atom_model.simulate_noisy_h0(omega = x * omega_ref, h_ref = h_ref, 
              noise_h0 = 0, nb_repeat = 1, s = s, nb_period = 1, verbose = True)
+# noise average value obtained over 20 repetitions
 f_noisy = lambda x: atom_model.simulate_noisy_h0(omega = x * omega_ref, h_ref = h_ref, 
             noise_h0 = 0.1, nb_repeat = 20, s = 40, nb_period = 1, verbose = True)
+# noise average value obtained over 1000 repetitions
 f_testing = lambda x: atom_model.simulate_noisy_h0(omega = x * omega_ref, h_ref = h_ref, 
             noise_h0 = 0.1, nb_repeat = 1000, s = 40, nb_period = 1, verbose = True)
 
+# function to extract the optimal parameters obtained by the optimizer 
 def _get_best_exp_from_BO(bo):
     """ From BO extract the best x"""
     Y_pred = bo.model.predict(bo.X)
     return bo.X[np.argmin(Y_pred[0])], np.min(Y_pred)
 
+
+
 #-----------------------------------------------------------------------------#
 # Optimization of the noiseless set-up
 #-----------------------------------------------------------------------------#
+# Bayesian Optimization
 myOpt = GPyOpt.methods.BayesianOptimization(f, bounds, **options_bo)
 myOpt.run_optimization(max_iter)
 x_no_noise, _ = _get_best_exp_from_BO(myOpt)
 
-## Another optimizer (differential evolution)
+# Another optimizer: differential evolution
 resultOptim = sco.differential_evolution(f, [limits], popsize=5)
 print(resultOptim)
 x_no_noise_de = resultOptim['x']
+
 
 #-----------------------------------------------------------------------------#
 # Optimization of the noisy set-up
@@ -67,10 +75,10 @@ x_noisy, _ = _get_best_exp_from_BO(myOpt)
 resultOptim = sco.differential_evolution(f_noisy, [(0.8, 1.2)], popsize=5, maxiter = 300)
 x_noisy_de = resultOptim['x']
 
+
+
 #-----------------------------------------------------------------------------#
-# Test the different optimal parameters found under the noisy set-up but with way 
-# more samples
-# Which one is the best
+# Test the different optimal parameters found 
 #-----------------------------------------------------------------------------#
 res_no_noise_BO = f_testing(x_no_noise)
 res_no_noise_DE = f_testing(x_no_noise_de)
